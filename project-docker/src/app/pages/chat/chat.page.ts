3217034b90f07ce0e152750d64b8ffc2391a5701
Pierre PERRIER-RIDET <pierre.perrierridet@groupepvcp.com>
2021-02-28 23:52:26 +0100

    premier rendu

--- project-docker/src/app/pages/chat/chat.page.ts
+++ project-docker/src/app/pages/chat/chat.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Messages } from '../../models/messages';
+import { MessagesService  } from '../../services/messages.service';
+
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  messages : Messages[]
+
+  constructor(private messageService: MessagesService) { }
 
   ngOnInit() {
+
+    this.messages = this.messageService.messages;
   }
 
 }
